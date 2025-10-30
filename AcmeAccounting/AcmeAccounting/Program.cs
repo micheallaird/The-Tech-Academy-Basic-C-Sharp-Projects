1290ef494a187ecd717e62f1c87bdd30f47e0e92
micheallaird <micheallaird@yahoo.com>
2025-10-29 19:49:11 -0600

    Acme Accounting Project

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Acme Accounting Systems" +
+    "\nRemember, we're \"accounting\" on you!");
+Console.ReadLine();
