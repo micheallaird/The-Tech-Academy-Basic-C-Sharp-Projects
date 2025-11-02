ff70d0492a989cf724c5ec12b01956ecca3b38be
micheallaird <micheallaird@yahoo.com>
2025-11-01 21:44:54 -0600

    Boolean Logic Submission

---
+++ BooleanLogicSubmission/BooleanLogicSubmission/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Car Insurance Age Verification
+Console.WriteLine("Welcome to the Car Insurance Eligibility Checker!");
+Console.WriteLine("What is your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Have you ever had a DUI? (true/false)");
+bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("How many speeding tickets do you have?");
+int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+if (age < 15 || hasDUI || speedingTickets > 3)
+{
+    Console.WriteLine("You are not eligible for car insurance.");
+}
+else
+{
+    Console.WriteLine("You are eligible for car insurance.");
+} Console.ReadLine();
+
