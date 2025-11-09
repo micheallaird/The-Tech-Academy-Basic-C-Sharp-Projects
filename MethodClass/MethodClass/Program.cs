1ce798ab6ea97dc8796d27cb4d61c1bf787ac8cd
micheallaird <micheallaird@yahoo.com>
2025-11-09 15:32:05 -0700

    MethodClass Submission Assignment

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using MethodClass;
+using System.Reflection;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiates the VoidMethodClass
+        VoidMethodClass mathOps = new VoidMethodClass();
+        // Calls the Void method with 2 integers as parameters
+        mathOps.AddAndDisplay(firstNumber: 15, secondNumber: 20);
+        // Keeps the console window open
+        Console.ReadLine();
+    }
+}
+
+
---
+++ MethodClass/MethodClass/VoidMethodClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MethodClass
+{
+    // Creates a class with a void method that takes two integers as parameters.
+    public class VoidMethodClass
+    {
+        public void AddAndDisplay(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + 10; // Example math operation on the first integer but does not use it further
+            Console.WriteLine(secondNumber); // Displays the second integer
+        }
+    }
+}
