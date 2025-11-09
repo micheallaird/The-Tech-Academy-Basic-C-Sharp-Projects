3ddee07a7c91f99dfe86ac79e64980b8a4e6f893
micheallaird <micheallaird@yahoo.com>
2025-11-09 13:55:39 -0700

    Added three distinct methods to the CallingMethods assignment

--- CallingMethods/CallingMethods/Math.cs
+++ CallingMethods/CallingMethods/Math.cs
@@ -8,19 +8,44 @@ namespace CallingMethods
 {
     public class Math
     {
-        // Takes one integer from the user and two additional pre-programmed integers
-        public Math(int num1, int num2, int num3)
+        // Takes one integer from the user and uses two additional pre-programmed integers
+        public Math(int num1)
         {
-            // Performs addition, multiplication, and subtraction with the three numbers
-            int addition = num1 + num2 + num3;
-            int multiplication = num1 * num2 * num3;
-            int subtraction = num1 - num2 - num3;
+            int num2 = 5;
+            int num3 = 10;
+            // Calls each method in turn, passing the user input to the method and displays returned integer to the console.
             // Tells the user what is happening with their input
             Console.WriteLine("Performing math operations on your numbers...");
-            // Displays the results of the math operations
-            Console.WriteLine($"Addition: {addition}");
-            Console.WriteLine($"Multiplication: {multiplication}");
-            Console.WriteLine($"Subtraction: {subtraction}");
+            Console.WriteLine("Press Enter for first equation.");
+            Console.ReadLine();
+            // Calls the Add method
+            int sum = Add(num1, num2, num3);
+            Console.WriteLine($"The sum of {num1} and {num2} and {num3} is: {sum}");
+            Console.WriteLine("Press Enter for second equation.");
+            Console.ReadLine();
+            // Calls the Subtract method
+            int difference = Subtract(num1, num2, num3);
+            Console.WriteLine($"The difference when {num2} and {num3} is subtracted from {num1} is: {difference}");
+            Console.WriteLine("Press Enter for third equation.");
+            Console.ReadLine();
+            // Calls the Multiply method
+            int product = Multiply(num1, num2, num3);
+            Console.WriteLine($"The product of {num1}, {num2}, and {num3} is: {product}");
+        }
+        // The add method that takes the user inputted integer and adds it to two other integers
+        private int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+        // The subtract method that takes the user inputted integer and subtracts two other integers from it
+        private int Subtract(int num1, int num2, int num3)
+        {
+            return num1 - num2 - num3;
+        }
+        // The multiply method that takes the user inputted integer and multiplies it by two other integers
+        private int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
         }
     }
 }
--- CallingMethods/CallingMethods/Program.cs
+++ CallingMethods/CallingMethods/Program.cs
@@ -12,15 +12,12 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            // Asks the user for three numbers to use in a math equation
-            Console.WriteLine("Enter the first number to use in the math equation:");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number to use in the math equation:");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the third number to use in the math equation:");
-            int num3 = Convert.ToInt32(Console.ReadLine());
-            // Performs the equation using the user's number and two additional pre-programmed numbers
-            Math math = new Math(num1, num2, num3); 
+            // Asks the user for a number to use in the methods
+            Console.WriteLine("Please enter an integer:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            // Instantiates the Math class, passing the user input to the constructor
+            Math mathOperations = new Math(userInput);
+            // Adds ReadLine to prevent the console from closing
             Console.ReadLine();
         }
     }
